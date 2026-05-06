1fab1a5fd587d51694cf3488394def6296e3399c
BugHunterAV <andre.valerio@alunos.fho.edu.br>
2026-05-05 21:52:43 -0300

    Update/Add postgres

--- app/settings.py
+++ app/settings.py
@@ -89,8 +89,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'manutencao',        
+        'USER': 'postgres',     
+        'PASSWORD': 'segredo',   
+        'HOST': 'localhost',                
+        'PORT': '5432',                     
     }
 }
 
